31aa273e84dcf052cbb008e39dfd58587d10ee36
Gregory Schultz <gregory.schultz@embarqmail.com>
2016-12-02 02:41:29 -0500

    Modified cmake batch script.

--- run_cmake_cbtf.sh
+++ run_cmake_cbtf.sh
@@ -1,3 +1,5 @@
+#!/bin/bash 
+
 rm -rf  build
 mkdir build
 pushd build
@@ -5,9 +7,10 @@ pushd build
 export CC=gcc
 export CXX=g++
 
-export KRELL_ROOT=/opt/DEVEL/krellroot_v2.2.3
-export CBTF_ROOT=/opt/DEVEL/cbtf_v2.2.4
-export MRNET_ROOT=/opt/DEVEL/krellroot_v2.2.3
+export KRELL_ROOT=/opt/STABLE/krellroot_v2.3.0
+export CBTF_ROOT=/opt/STABLE/cbtf_v2.3.x
+export MRNET_ROOT=/opt/STABLE/krellroot_v2.3.0
+export BOOST_ROOT=/opt/boost-1.59.0
 
 cmake .. \
         -DCMAKE_BUILD_TYPE=None \
@@ -15,7 +18,8 @@ cmake .. \
         -DCMAKE_C_FLAGS="-g -O2" \
         -DCMAKE_INSTALL_PREFIX=${CBTF_ROOT} \
         -DCMAKE_PREFIX_PATH=${KRELL_ROOT} \
-        -DMRNET_DIR=${MRNET_ROOT}
+        -DMRNET_DIR=${MRNET_ROOT} \
+	-DCBTF_BOOST_ROOT=_ROOT=${BOOST_ROOT}
 
 make clean
 make
